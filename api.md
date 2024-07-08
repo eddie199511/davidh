8d8d165bdfd07392ef4ba47db01e19db954f0d7a
eam8881 <eddie.martinez717@gmail.com>
2024-07-08 09:47:37 -0600

    Refine API response handling in call_api.py
    Enhanced API response handling in `call_api.py` script. The changes include printing the JSON response as a dictionary and then accessing specific data from the response dictionary. Modified emphasis in `api.md` from bold to italics for 'requests' python library.

--- api.md
+++ api.md
@@ -7,5 +7,4 @@
    python call_api.py
     ```
 3. This invokes a GET request to a simple, public API.
-4. The example shows usage of the **requests** python library. You can read more about the requests library [here](https://realpython.com/python-requests/)
-5. 
+4. The example shows usage of the *requests* python library. You can read more about the requests library [here](https://realpython.com/python-requests/)
--- src/call_api.py
+++ src/call_api.py
@@ -6,4 +6,15 @@ response = requests.get(url)
 
 print(response.status_code)
 
-print(response.json())
+response_dict = response.json()
+
+print(response_dict)
+
+# The response will look like this:
+# {'userId': 1,
+#  'id': 1,
+#  'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+#  'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit'}
+
+# As we learned in dictionaries, we can access data from the response like so:
+print(response_dict['title'])
